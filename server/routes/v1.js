82748708a5ee7c7f3b3a4ee6972cf838bb49f927
dropella-dev <austinandogola@gmail.com>
2025-06-20 16:23:42 +0300

    modern, test ready

--- server/routes/v1.js
+++ server/routes/v1.js
@@ -3,12 +3,13 @@ const {askforEvents,askForCompany}=require('../utils/extension')
 
 
 router.get('/events/meetup',async(req,res)=>{
+    console.log('Req received')
     const queryParams=req.query
     // const {keyword,country,fromDate,toDate,size,page,sort,location,category} = queryParams
     queryParams.scrapeType = 'meetup'
      let events = await askforEvents(queryParams)
 
-    console.log("Events",events,'evetns')
+    console.log("Events",events.length,'evetns')
 
     res.json({events}).status(200)
 })
